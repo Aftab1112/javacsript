3b905def787fc5f66366e29385b3f37a69c3715a
Aftab1112 <you@example.com>
2023-12-21 15:54:19 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,62 @@
-let a = 89;
-console.log(a);
-a = "aftab";
-console.log(a);
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b :",a+b);
+// console.log("a - b :",a-b);
+// console.log("a * b :",a*b);
+// console.log("a / b :",a/b);
+// console.log("a % b :",a%b);
+// console.log("a ** b :",a**b);
+
+ // Unary operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ",a , "b = ",b);
+// console.log("a-- = ",a--)
+// console.log(a);
+
+// a-= 10  
+// console.log(a)
+
+//Comparison ooerators
+
+// let a = 5
+// let b = "5"
+
+// console.log("a!==b",a!==b)
+
+//Logical opreators
+
+// let a = 6;
+// let b = 5;
+// console.log("6<5",!(a===b));
+
+//Conditional statements 
+
+// let age = 25;
+
+// if(age>=18){
+//     console.log("You can vote")
+// }
+
+// let mode = "dark"
+// let colour;
+
+// if(mode === "dark"){
+//     colour = "black"
+// } else if(mode === "blue"){
+//     colour = "blue"
+// }else if(mode === "pink"){
+//     colour = "pink"
+// }else{
+//     colour = "white"
+// }
+
+// console.log(colour)
+
+
+
+age > 18 ? "is adult" : "not adult"
+console.log(age)
